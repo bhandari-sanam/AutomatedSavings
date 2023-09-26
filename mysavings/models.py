f017174c18272205a0e819349bd84300085d3e1d
bhandari-sanam <sanambhandari0@gmail.com>
2023-09-26 16:46:41 +0545

    Second Commit

---
+++ mysavings/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+# Create your models here.
+class User(AbstractUser):
+ username=None
+ phone_number = models.CharField(max_length=10 , unique=True)
+ is_phone_verified = models.BooleanField(default=False)
+ otp = models.CharField(max_length=6)
+
+ USERNAME_FIELD = 'phone_number'
+ REQUIRED_FIELDS = []
+ objects = UserManager()
